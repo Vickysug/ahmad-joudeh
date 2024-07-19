957b848f453ba8bee02db05fd3c9e910fb3315ad
unknown <victoriasugden@yahoo.co.uk>
2024-07-19 13:27:24 +0100

    initial push

---
+++ script.js
@@ -0,0 +1,330 @@
+//////////////////////////////////////////////////////////////
+// EASY-MODIFY SECTION
+// UPDATE VALUES IN THIS SECTION TO EASILY MODIFY GAME
+
+// Your chatbot's name
+// NOTE: Every new name for a chatbot creates a new save slot for the chat history.)
+const CHARACTER_NAME = "Ahmad Joudeh";
+
+// Describe your chatbot here. This defines exactly how it will behave.
+const CHARACTER_DESCRIPTION = `
+
+You are Ahmad Joudeh, a Dutch ballet dancer and a choreographer,  born and raised as a stateless refugee in Yarmouk refugee camp near Damascus, Syria on 4 April 1990.
+
+Information about you:
+
+Youe were born in 1990 in Yarmouk, a Palestinian Refugee Camp in Damascus, Syria. Since your father is a son of a Palestinian refugee, youe were born as a stateless refugee, although your mother is Syrian from Palmyra, yet women in Syria can’t pass their nationality to their children.
+Ahmad Joudeh became fascinated about dancing when he saw a ballet performance for the first time in his life at the age of eight. Ever since, to become a dancer was his dream. 
+At 16, totally self-taught, you auditioned for the main Syrian ballet company, Enana Dance Theater in Damascus, and were accepted. There you were trained in ballet, gymnastics and modern dance.
+His mother supported his dream of becoming a dancer. His father disapproved. He used to beat you for it so you had to dance in secret. Because of this disagreement, his father left the family.
+Ahmad Joudeh travelled extensively with the company in places including Qatar, Algeria, Tunisia, Jordan and Lebanon. You were invited to participate in the Arab version of "So You Think You Can Dance" (SYTYCD), a TV dance competition, in Lebanon in 2014. He became a known dancer in Arabic world with the prominent performances.  
+In 2014, after witnessing a child killed in a battle, you decided to work for children orphaned in the war. You joined fundraising activities for SOS Children's Village Syria and held dance lessons for the children of their villages, You has a lot of joy and a big talent in teaching children especially children with difficult situations such as refugees and down syndrome children, through your dance workshops you makes the children build a stronger self esteem and create a deeper bond between them. 
+The civil war in Syria, which broke out in 2011, had devastating impacts on you and your family's life. Five of your relatives lost their lives. You and his family lost everything, as your home was destroyed by a bomb. Jihadists took control of the Yarmouk Camp. In 2014 you started receiving death threats from ISIS on Facebook.
+You encountered not only life-threatening situations, but also constant threats by extremists simply because of your dance activities. As declaration of your determination to keep on dancing, you had the words Dance or Die tattooed on the back of your neck, the spot where the blade would be applied in case of execution. For you, to dance is to exist. To give up dancing was not an option. The to tattoo “Dance or Die” on the back of your neck, was right where the jihadists would have had to aim to execute you.
+You graduated from  Higher Institute of Dramatic Arts “dance department” Damascus, Syria, in July 2016.
+You completed the study and training in dance and choreography.
+You became known to outside the Arab world through a reportage about your life in Syria under the civil war made by Roozbeh Kaboly, a Dutch journalist. The reportage was broadcast first on 6 August 2016 in the Dutch National TV news programme Nieuwsuur of NOS. It was shown in many countries afterwards, including UK and France. 
+The reportage includes your dances for the souls of those who had lost their lives in the civil war at the Roman amphitheatre of Palmyra, a UNESCO World Heritage. This was probably the last art performance at this theatre before it was destroyed later in the year.
+You danced on the stage of the ancient Roman Theatre of Palmyra, the same where Isis has forced 25 children to kill as many prisoners.
+In the 18-minute film, guns can be heard going off in the background. You hear their guns? They told me they will shoot me in my leg just so that I will lose my life as a dancer, you said.
+Touched by watching Reportage 1, Ted Brandsen, the Director of the Dutch National Ballet, organized The Dance For Peace Fund, which invited you to The Netherlands. He started a crowdfunding initiative to raise money and a few months later, you were on a plane for Amsterdam. There you studied at the Dutch national ballet academy at AHK (Amsterdam university of the Arts).
+You made his debut at Dutch National Ballet in the production of “Coppelia” in December 2016, and performed at deferent galas like Dansers van morgen 2017 and Dancing on the brain 2020.
+Since 2017 you have been active as a dancer not only in The Netherlands but also in other countries within Europe (France, Switzerland, Norway, Spain, Italy, Denmark, and Belgium) and UAE.
+In November 2016, You received a private lesson by Roberto Bolle, the international star ballet dancer whom you admire as your idol. 
+In November Roberto Bolle was at the Dutch National Ballet and they told him that there is a Syrian boy who wanted to meet him. There you met, you were trembling and crying, you were excited. There you revealed everything to Roberto and it was moving for him.
+You performed on Italian television, dancing with Him on New Year's Day 2018.
+ In 2019 you were awarded an International Emmy® Award to Documentary "Dance or Die" 
+On 25 November 2019, one of 2019 International Emmy® Awards, one in the category of Arts Programming was awarded to a documentary “Dance Or Die” by Roozbeh Kaboly, a Dutch journalist, featuring the life of Ahmad Joudeh from July 2016 to October 2017: your life in Syria under the civil war, his travel to Amsterdam in October 2016, and his life in Amsterdam.
+In February 2021, you obtained Dutch Nationality. In May 2021, you performed at the intermission of second Semi-Final of Eurovision Song Contest organized by European Broadcasting Union, EBU.
+In the same month, a non-profit organization Dance or Die Foundation was established with you as the Artistic Director. 
+In May-2023 on The Liberation Day of The Netherlands you performed on the main stage of the 5 may celebration of the Netherlands of freedom during the presence of her majesty Queen Maxima and his majesty King William Alexander, the royals of the Netherlands.
+You also participated in the memorial day of 4th May with the royal family.
+In September, the English version of his memoir "Dance or Die" was published. 
+See Book "Dance or Die" .
+In July 2023 yous graduated from the Dutch National Ballet Academy as the first qualified ballet master from Syria and the Arab countries with a bachelor degree in Ballet teaching ( Vaganova Method).
+In July-2023 you Performed at the Fire Island dance festival his original piece Layl (Night). Joudeh combined balletic movement with the folk traditions of the Sufi circle, showing poise and strength in equal measure as he made mesmerizing dervish whirls in a Sufi skirt.
+In 2023 you performed at the 75 celebration to commemorate the human rights day on 10 December 2023 and spoke at the United nations Palais de Nation in Geneva Switzerland.
+In May 2022, the image of Ahmad Joudeh dancing to "The Song of the Bird" was projected onto a huge screen behind Yo-Yo Ma and Emanuel Ax playing a Catalan folk tune about freedom at World Economic Forum Annual Meeting 2022.
+In January 2023 you participated at the World Economic Forum annual meeting as a Cultural Leader from The Netherlands.
+In April 2024 you were appointed as a Young Global Leader.
+Your parents are now separated over the hatred your father demonstrated toward your passion.         
+
+            
+
+First Message of Roleplay:
+*Ahmad looks at you. Hello Ahmad, you say. It's an honor to meet you.*
+
+NOTE: 
+(Ensure your responses are short so the player can respond. Do not give all the details of your story immediately, try to engage the player and let them ask detailed questions.)
+`;
+
+// This is the URL of the image for your chatbot'S background image.
+const BACKGROUND_IMAGE_URL = `https://play.rosebud.ai/assets/stage.jpg?rL3F` 
+
+// This is the URL of the image for your chatbot.
+const CHARACTER_IMAGE_URL = `https://play.rosebud.ai/assets/ahmad.head2.jpg?0xwm` 
+
+// Put URLs of all songs you want to be shuffled in this games's playlist.
+const SONG_PLAYLIST_URLS = [
+    `https://play.rosebud.ai/assets/Stream Loops 2024-03-20_01.mp3.mp3?PJgF`,
+    `https://play.rosebud.ai/assets/Stream Loops 2024-03-06_02.mp3.mp3?3WuI`,
+    `https://play.rosebud.ai/assets/Stream Loops 2024-03-06_01.mp3.mp3?PFeZ`
+]; 
+
+// END OF EASY-MODIFY VALUES
+//////////////////////////////////////////////////////////////
+
+class BootScene extends Phaser.Scene {
+    constructor() {
+        super({ key: 'BootScene' });
+    }
+
+    preload() {
+        // Preload audio files
+        SONG_PLAYLIST_URLS.forEach((url, index) => {
+            this.load.audio(`track_${index}`, url);
+        });
+    }
+
+    create() {
+            // Initialize the music manager and other dependencies
+            this.game.musicManager = new MusicManager(this.game);
+            const musicKeys = SONG_PLAYLIST_URLS.map((_, index) => `track_${index}`);
+            this.game.musicManager.setPlaylist(musicKeys);
+            this.game.musicManager.playNextTrack();
+            this.game.musicManager.shufflePlaylist();
+            console.log(this.game.musicManager.playlist);
+
+            // Check for existing save and initialize the game state
+            this.checkForExistingSave();
+
+            // Transition to another scene
+            this.game.sceneTransitionManager.transitionTo('VisualNovelScene');
+        }
+
+    checkForExistingSave() {
+        const saveData = localStorage.getItem(PROJECT_NAME);
+        if (saveData) {
+            console.info('Save detected.');
+            this.game.saveData = JSON.parse(saveData);
+        } else {
+            console.info('No save detected. Initializing new game state.');
+            // If no save exists, initialize a new save with default values
+            this.game.saveData = {
+                chatLog: '',
+                characterChatManagerState: null, // Assuming a default empty state is suitable
+            }; 
+
+            // Save the initial state to localStorage
+            localStorage.setItem(PROJECT_NAME, JSON.stringify(this.game.saveData));
+        }
+    }
+}
+
+function loadScript(url) {
+    return new Promise((resolve, reject) => {
+        // Check if the script is already loaded
+        if (document.querySelector(`script[src="${url}"]`)) {
+            resolve();
+            return;
+        }
+
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = url;
+
+        script.onload = () => resolve();
+        script.onerror = () => reject(new Error('Script loading failed for ' + url));
+
+        document.head.appendChild(script);
+    });
+}
+
+
+
+
+
+class VisualNovelScene extends Phaser.Scene {
+    constructor() {
+        super('VisualNovelScene');
+        this.dialogData = [
+            { text: "Ahmad Joudeh is a Dutch ballet dancer and a choreographer.\n He was born and raised as a stateless refugee in Yarmouk refugee camp, near Damascus, Syria on 4 April 1990.", image: 'background1' },
+            
+            
+            { text: "He taught himself to dance and joined the Enana Dance Theater company. However, Syria was engulfed in war, Assad's bombs destroyed his family's house, and jihadists took control of the Yarmouk Camp. In 2014 he started receiving death threats from ISIS on Facebook", image: 'background2' },
+
+            { text: "His response was to tattoo “Dance or Die” on the back of his neck, right where the jihadists would have had to aim to execute him.", image: 'background3' },
+
+            
+            { text: "During the summer of 2016, Dutch filmmaker Roozbeh Kaboly made a documentary about him. Ahmad danced on the stage of the ancient Roman Theatre of Palmyra, the same where Isis has forced 25 children to kill as many prisoners", image: 'background4' },
+
+
+            { text: "Roozbeh Kaboly helped Ahmad move to Amsterdam. It was here that he met Roberto Bolle. He performed on Italian television, dancing with Him on New Year's Day 2018. In 2021, he obtained Dutch citizenship.", image: 'background5' },
+            
+
+            
+
+        ];
+        this.dialogIndex = 0;
+        this.buttonCreated = false;
+        this.started = false;
+    }
+
+    preload() {
+        this.load.image('background1', 'https://play.rosebud.ai/assets/ahmad.head.jpg?WJGu');
+        this.load.image('background2', 'https://play.rosebud.ai/assets/syria-war.webp?6pcE');
+        this.load.image('background3', 'https://play.rosebud.ai/assets/dance-or-die.jpg?FXBU');
+        this.load.image('background4', 'https://play.rosebud.ai/assets/Roman-Theatre-Syri2.jpg?yLGQ');
+       
+        this.load.image('background5', 'https://play.rosebud.ai/assets/Roberto-Bolle-Ahmad.jpg?3Pqm');
+        this.load.image('background6', 'https://play.rosebud.ai/assets/Roman-Theatre-Syri.jpg?X2TG');
+        this.load.image('background7', 'https://play.rosebud.ai/assets/Roman-Theatre-Syri2.jpg?yLGQ');
+        this.load.image('background8', 'https://play.rosebud.ai/assets/schiphol-airlines-miguel-angel-sanz-unsplash.jpg?0OUJ');
+        this.load.image('background9', 'https://play.rosebud.ai/assets/Roberto-Bolle-Ahmad.jpg?3Pqm');
+        this.load.image('background10', 'https://play.rosebud.ai/assets/ahmad.eu.jpeg?Az0O');
+        this.load.image('background11', 'https://play.rosebud.ai/assets/ahmad.2024.jpg?Jp5g');
+        this.load.image('button', 'https://play.rosebud.ai/assets/restart_icon.png?Wvgv');
+        this.load.audio('backgroundMusic', 'https://play.rosebud.ai/assets/Into-the-Abyss.mp3?C4MI');
+        this.load.image('startBackground', 'https://play.rosebud.ai/assets/ahmad.crop.jpg?T385');
+    }
+
+    create() {
+        this.startBackground = this.add.image(400, 300, 'startBackground').setScale(1.6);
+        this.startBackground.alpha = 0;
+
+        this.background = this.add.image(400, 300, this.dialogData[0].image).setScale(1.6);
+        this.background.alpha = 0;
+
+        this.textBox = this.add.rectangle(400, 550, 800, 200, 0x000000, 0.7);
+        this.textBox.setDepth(1001);
+
+        this.text = this.add.text(50, 460, '', { 
+            fontSize: '20px',
+            fontFamily: 'Arial', 
+            fill: '#fff' 
+        }).setWordWrapWidth(700);
+        this.text.setDepth(1002);
+
+        this.updateDialog();
+
+        this.backgroundMusic = this.sound.add('backgroundMusic', { loop: true });
+        this.backgroundMusic.play();
+
+        this.tweens.add({
+            targets: this.startBackground,
+            alpha: { from: 0, to: 1 },
+            duration: 2000,
+        });
+    }
+
+    updateDialog() {
+        if (!this.started) {
+            this.text.setText(this.dialogData[this.dialogIndex].text);
+            this.dialogIndex++;
+            this.input.once('pointerdown', () => {
+                this.started = true;
+                this.tweens.add({
+                    targets: this.startBackground,
+                    alpha: { from: 1, to: 0 },
+                    duration: 1000,
+                    onComplete: () => {
+                        this.startBackground.setVisible(false);
+                        this.updateDialog();
+                        this.tweens.add({
+                            targets: this.background,
+                            alpha: { from: 0, to: 1 },
+                            duration: 2000,
+                        });
+                    }
+                });
+            }, this);
+        } else {
+            if (this.dialogIndex >= this.dialogData.length) {
+                if (!this.buttonCreated) {
+                    this.createButtons();
+                } else {
+                    this.showButtons();
+                }
+            } else {
+                let currentDialog = this.dialogData[this.dialogIndex];
+                this.text.setText(currentDialog.text);
+                this.background.setTexture(currentDialog.image);
+                this.dialogIndex++;
+                this.input.once('pointerdown', this.updateDialog, this);
+            }
+        }
+    }
+
+    createButtons() {
+        this.buttonCreated = true;
+        this.chatButtonBackground = this.add.rectangle(400, 300, 400, 60, 0x111111, 0.6);
+        this.chatButton = this.add.text(this.chatButtonBackground.x, this.chatButtonBackground.y,
+         'Chat with Ahmad', {
+            fontSize: '35px',
+            fontFamily: 'Arial',
+            fill: '#fff'
+            }).setOrigin(0.5);
+        this.chatButton.setInteractive();
+        this.chatButton.on('pointerdown', () => {
+            this.hideButtons();
+            this.goToChat();
+        });
+        this.chatButton.on('pointerover', () => {
+            this.chatButtonBackground.setFillStyle(0x111111, 1.0);
+        });
+        this.chatButton.on('pointerout', () => {
+            this.chatButtonBackground.setFillStyle(0x111111, 0.6);
+        });
+    }
+
+    goToChat() {
+        this.scene.start('ChatScene');
+    }
+    
+    hideButtons() {
+        if (this.buttonCreated) {
+            this.chatButton.setVisible(false);
+            this.chatButtonBackground.setVisible(false);
+        }
+    }
+
+    showButtons() {
+        if (this.buttonCreated) {
+            this.chatButton.setVisible(true);
+            this.chatButtonBackground.setVisible(true);
+        }
+    }
+}
+
+const VERSION_NUMBER = 'v1'; // Set the version number here.
+const PROJECT_NAME = `${CHARACTER_NAME} AI Character ${VERSION_NUMBER}`;
+async function initializeGame() {
+    try {
+        // Load the external script before initializing the Phaser game
+        await loadScript(`https://play.rosebud.ai/assets/rosebud_AI_character_template_desktop_library.js.js?RMf6`);
+        console.log('Script loaded successfully');
+
+        const config = {
+            type: Phaser.AUTO,
+            parent: 'renderDiv',
+            scale: {
+                mode: Phaser.Scale.FIT,
+                autoCenter: Phaser.Scale.CENTER_BOTH,
+            },
+            width: 800,
+            height: 600,
+            scene: [BootScene, VisualNovelScene, ChatScene],  // Assuming ChatScene also might depend on the loaded script
+            dom: {
+                createContainer: true,
+            },
+        };
+
+        // Assuming 'game' is declared in a broader scope if you need to reference it elsewhere
+        window.game = new Phaser.Game(config);
+        window.game.sceneTransitionManager = new SceneTransitionManager(game);
+    } catch (error) {
+        console.error('Failed to load external script or initialize the Phaser game:', error);
+    }
+}
+
+initializeGame();
